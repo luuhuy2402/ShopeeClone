cceab2cbf46d101d33a7ad14dec748977413be80
luuhuy2402 <xeoxeo1895@gmail.com>
2024-12-29 21:22:48 +0700

    Custom class .container Tailwindcss bằng cách tạo plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    // Loại bỏ thuộc tính tailwind của container
+    corePlugins: {
+        container: false,
+    },
     theme: {
         extend: {
             colors: {
@@ -8,5 +14,25 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addComponents, theme }) {
+            addComponents({
+                // ".container": {
+                //     maxWidth: "80rem",
+                //     marginLeft: "auto",
+                //     marginRight: "auto",
+                //     paddingLeft: "1rem",
+                //     paddingRight: "1rem",
+                // },
+
+                ".container": {
+                    maxWidth: theme("columns.7xl"),
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    paddingLeft: theme("spacing.4"),
+                    paddingRight: theme("spacing.4"),
+                },
+            });
+        }),
+    ],
 };
