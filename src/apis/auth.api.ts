f71e2d4e8371e4254edcef538790f7cd7c5f9bf1
luuhuy2402 <xeoxeo1895@gmail.com>
2025-01-01 17:30:22 +0700

    Xử lí lỗi đơn thuần message vs axios interceptor

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -2,4 +2,4 @@ import { AuthResponse } from "../types/auth.type";
 import http from "../utils/http";
 
 export const registerAccount = (body: { email: string; password: string }) =>
-    http.post<AuthResponse>("/register", body);
+    http.post<AuthResponse>("/registerr", body);
