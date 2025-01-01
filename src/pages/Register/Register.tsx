4ac42fcc28f36bfb0aca755ac42b37814e04dcd7
luuhuy2402 <xeoxeo1895@gmail.com>
2025-01-01 17:12:59 +0700

    xử lí lỗi 422 server trả về

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -6,13 +6,15 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useMutation } from "@tanstack/react-query";
 import { omit } from "lodash";
 import { registerAccount } from "../../apis/auth.api";
+import { isAxiosUnprocessableEntityError } from "../../utils/utils";
+import { ResponseApi } from "../../types/utils.type";
 
 type FormData = Schema;
 export default function Register() {
     const {
         register,
         handleSubmit,
-
+        setError,
         formState: { errors },
     } = useForm<FormData>({ resolver: yupResolver(schema) });
 
@@ -25,6 +27,44 @@ export default function Register() {
         registerAccountMutation.mutate(body, {
             onSuccess: (data) => {
                 console.log(data);
+            },
+            onError: (error) => {
+                if (
+                    isAxiosUnprocessableEntityError<
+                        ResponseApi<Omit<FormData, "confirm_password">>
+                    >(error)
+                ) {
+                    const formError = error.response?.data.data;
+                    if (formError) {
+                        Object.keys(formError).forEach((key) => {
+                            setError(
+                                key as keyof Omit<FormData, "confirm_password">,
+                                {
+                                    message:
+                                        formError[
+                                            key as keyof Omit<
+                                                FormData,
+                                                "confirm_password"
+                                            >
+                                        ],
+                                    type: "Server",
+                                }
+                            );
+                        });
+                    }
+                    // if (formError?.email) {
+                    //     setError("email", {
+                    //         message: formError.email,
+                    //         type: "Server",
+                    //     });
+                    // }
+                    // if (formError?.password) {
+                    //     setError("password", {
+                    //         message: formError.password,
+                    //         type: "Server",
+                    //     });
+                    // }
+                }
             },
         });
     });
